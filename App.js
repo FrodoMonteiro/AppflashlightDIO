73092963234757e780bbed7e9092397aebf01839
Frodomonteiro <henriquebk20@gmail.com>
2022-06-29 20:35:22 -0300

    appflash

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
-export default function App() {
+
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(()=>{
+    Torch.switchState(toggle);
+  }, [toggle])
+  ;
+
+  useEffect(()=>{
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+    return () => subscription.remove();
+  },[]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View style={toggle ? style.containerLight : style.container}>
+  <TouchableOpacity 
+    onPress = {handleChangeToggle}>
 
-const styles = StyleSheet.create({
+    <Image style={toggle ? style.lightingOn : style.lightingOff}
+      source={
+        toggle
+        ? require('./assets/icons/eco-light.png')
+        : require('./assets/icons/lampada.png')
+      }
+    />
+    
+
+    <Image style={style.logoDio}
+      source={
+        toggle
+        ? require('./assets/icons/logo-dio.png')
+        : require('./assets/icons/logo-dio-white.png')
+      }
+    />
+    </TouchableOpacity>
+  </View>
+  );
+};
+
+export default App;
+
+
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  
   },
+
+  containerLight:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+   
+
+  },
+  
+   lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+
+  },
+  logoDio: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+
+  },
+  
 });
